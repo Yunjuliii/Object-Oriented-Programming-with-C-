7b559175093f05408fe9467236ca073c4f307adc
Yunju Li <144848625+Yunjuliii@users.noreply.github.com>
2024-09-24 05:31:02 +0800

    Add files via upload

---
+++ Module2/T2_4_Dog_class/build/CMakeFiles/dog_class.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dog_class.dir/src/dog_class.cpp.o"
+  "CMakeFiles/dog_class.dir/src/main.cpp.o"
+  "dog_class"
+  "dog_class.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dog_class.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
