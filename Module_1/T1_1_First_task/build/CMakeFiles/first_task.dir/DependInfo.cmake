e4a8f44a2aed591073df2d5ac9f7390a8e5cd1d0
Yunju Li <144848625+Yunjuliii@users.noreply.github.com>
2024-09-24 05:25:18 +0800

    Add files via upload

---
+++ Module_1/T1_1_First_task/build/CMakeFiles/first_task.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lyj/cpp-course/Module_1/T1_1_First_task/src/main.cpp" "/home/lyj/cpp-course/Module_1/T1_1_First_task/build/CMakeFiles/first_task.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Module_1/T1_1_First_task/build/CMakeFiles/first_task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_task.dir/src/main.cpp.o"
+  "first_task"
+  "first_task.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
